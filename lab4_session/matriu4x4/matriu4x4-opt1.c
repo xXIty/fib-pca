f41de26b10533388f5662a17af2083202c4d8ad0
Marc Clasca <marc.clasca@bsc.es>
2021-04-27 18:57:53 +0200

    Afegeix instrumentacio matriu4x4

--- lab4_session/matriu4x4/matriu4x4-opt1.c
+++ lab4_session/matriu4x4/matriu4x4-opt1.c
@@ -1,3 +1,12 @@
+#define _GNU_SOURCE
+#include <asm/unistd.h>
+#include <linux/perf_event.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include <inttypes.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -45,6 +54,18 @@ void print_matriu(int C[][4])
    }
 }
 
+
+static long
+perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
+                int cpu, int group_fd, unsigned long flags)
+{
+    int ret;
+
+    ret = syscall(__NR_perf_event_open, hw_event, pid, cpu,
+                    group_fd, flags);
+    return ret;
+}
+
 int main(int argc, char *argv[])
 {
     int A[4][4], B[4][4], C[4][4] = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
@@ -61,7 +82,39 @@ int main(int argc, char *argv[])
 	n_iter = atoi(argv[1]); 
     }
 
+    /*
+    struct perf_event_attr pe;
+    long long count;
+    int fd;
+     memset(&pe, 0, sizeof(struct perf_event_attr));
+    pe.type = PERF_TYPE_HARDWARE;
+    pe.size = sizeof(struct perf_event_attr);
+    pe.config = PERF_COUNT_HW_INSTRUCTIONS;
+    pe.disabled = 1;
+    pe.exclude_kernel = 1;
+    // Don't count hypervisor events.
+    pe.exclude_hv = 1;
+
+    fd = perf_event_open(&pe, 0, -1, -1, 0);
+    if (fd == -1) {
+        fprintf(stderr, "Error opening leader %llx\n", pe.config);
+        exit(EXIT_FAILURE);
+    }
+
+    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+    */
+
     multiplica(A, B, C, n_iter);
+
+    /*
+    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+    read(fd, &count, sizeof(long long));
+
+    printf("Used %lld instructions\n", count);
+
+    close(fd);
+    */
 
     print_matriu(C);
          

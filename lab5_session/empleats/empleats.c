aa5dc5ec4f1e3584f220b5889cb67181f157b55f
xXIty <miquel.roset@fib.upc.edu>
2021-05-12 17:40:14 +0200

    empleats vector containing only index

--- lab5_session/empleats/empleats.c
+++ lab5_session/empleats/empleats.c
@@ -27,45 +27,58 @@ typedef struct
 #endif
 } Templeat;
 
+typedef struct
+{
+  long long int NID;
+  unsigned int index;
+} Tindex;
+
 int compare(const void *p1, const void *p2)
 {
-    if (((Templeat*) p1)->NID > ((Templeat*) p2)->NID) return(1);
-    else if (((Templeat*) p1)->NID < ((Templeat*) p2)->NID) return(-1);
+    if (((Tindex*) p1)->NID > ((Tindex*) p2)->NID) return(1);
+    else if (((Tindex*) p1)->NID < ((Tindex*) p2)->NID) return(-1);
     else return(0);
-
 }
 
 int main(int argc, char *argv[])
 {
-	Templeat *empleats;
+	Templeat *empleats_data;
+  Tindex   *empleats_index;
+  
 	unsigned int N = 2000000, i;
 
 	if (argc > 1) N = atoi(argv[1]);
 
 
-	empleats = (Templeat *) malloc(N*sizeof(Templeat));
-	if (empleats == NULL) { fprintf(stderr, "Out of memory\n"); exit(0); }
-	memset(empleats, 0, N*sizeof(Templeat));
+	empleats_data  = (Templeat *) malloc(N*sizeof(Templeat));
+  empleats_index = (Tindex   *) malloc(N*sizeof(Tindex));
+	if (empleats_data == NULL) { fprintf(stderr, "Out of memory\n"); exit(0); }
+	memset(empleats_data, 0, N*sizeof(Templeat));
 
 	/* Random generation */
 	for (i=0; i<N; i++) {
-		empleats[i].NID = rand();
-		empleats[i].Nom[0] = 'A' + ((i>>3) + (i<<2)) & 0x15;
-		empleats[i].Cognoms[0] = 'a' + ((i>>2) + (i<<3)) & 0x15;
-		empleats[i].horesMes[i % MAX_DIAS_MES] = i;
-		empleats[i].Categoria[i % MAX_CATEGORIA] = (i << 2) * i;
-		empleats[i].ptrClauForaneaDepartament = i<<10 + i<<14 + i>>3;
-		empleats[i].ptrClauCategoria = i<<24 + i<< 18 + i<<10 + i>>2;
+    long long int NID = rand();
+  
+    empleats_index[i].index  =  i;
+    empleats_index[i].NID    =  NID;
+
+		empleats_data[i].NID = NID;
+		empleats_data[i].Nom[0] = 'A' + ((i>>3) + (i<<2)) & 0x15;
+		empleats_data[i].Cognoms[0] = 'a' + ((i>>2) + (i<<3)) & 0x15;
+		empleats_data[i].horesMes[i % MAX_DIAS_MES] = i;
+		empleats_data[i].Categoria[i % MAX_CATEGORIA] = (i << 2) * i;
+		empleats_data[i].ptrClauForaneaDepartament = i<<10 + i<<14 + i>>3;
+		empleats_data[i].ptrClauCategoria = i<<24 + i<< 18 + i<<10 + i>>2;
 #ifdef EXTES     
-                empleats[i].Address[0] = 'A' + ((i>>4) + (i<<2)) & 0x15;
-                empleats[i].Nom_parella[0] = 'a' + ((i>>5) + (i<<2)) & 0x15;
-  		empleats[i].Cognoms_parella[0] = 'A' + ((i>>5) + (i<<3)) & 0x15;
+      empleats_data[i].Address[0]          =  'A'  +  ((i>>4)  +  (i<<2))  &  0x15;
+      empleats_data[i].Nom_parella[0]      =  'a'  +  ((i>>5)  +  (i<<2))  &  0x15;
+      empleats_data[i].Cognoms_parella[0]  =  'A'  +  ((i>>5)  +  (i<<3))  &  0x15;
 #endif
 	}
 
-	qsort(empleats, N, sizeof(Templeat), compare);
+	qsort(empleats_index, N, sizeof(Tindex), compare);
 	for (i=0; i<N; i++){
-	        write(1, &empleats[i],sizeof(Templeat));
+	        write(1, &empleats_data[empleats_index[i].index],sizeof(Templeat));
 	}
 
 	return 0;

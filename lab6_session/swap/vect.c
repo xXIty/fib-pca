ab9faa712aedb0701fcfc44562c2cde560ba9197
xXIty <miquel.roset@fib.upc.edu>
2021-05-31 18:57:56 +0200

    Working version with 0.02598420 in moodle

--- lab6_session/swap/vect.c
+++ lab6_session/swap/vect.c
@@ -23,33 +23,31 @@ int main(int argc, char *argv[])
 
     int  n1;
     int  res;
-    __m256i *c1;
-    __m256i *c1_out;
+    __m128i *c1;
+    __m128i *c1_out;
+    __m128i mask;
 
-    posix_memalign((void *)&c1,      CACHE_ALIGNMENT,  count*sizeof(__m256i));
-    posix_memalign((void *)&c1_out,  CACHE_ALIGNMENT,  count*sizeof(__m256i));
+    posix_memalign((void *)&c1,      CACHE_ALIGNMENT,  count*sizeof(* c1));
+    posix_memalign((void *)&c1_out,  CACHE_ALIGNMENT,  count*sizeof(* c1));
 
-    // Vectorization code
-    __m256i mask, out;
-    mask = _mm256_set_epi8(
-            14, 15, 12, 13, 10, 11, 8, 9, 6, 7, 4, 5, 2, 3, 0, 1,
-            14, 15, 12, 13, 10, 11, 8, 9, 6, 7, 4, 5, 2, 3, 0, 1);
+    mask = _mm_set_epi8(14, 15, 12, 13, 10, 11, 8, 9, 6, 7, 4, 5, 2, 3, 0, 1);
 
-    while ((n1=read(0, c1, count*sizeof(__m256i))) > 0)
+    while ((n1=read(0, c1, count*sizeof(* c1))) > 0)
     {
         int i, j;
-        int c1_chars_left = n1 & 0xFF;
+        int c1_chars_left = n1 & 0xF;
         int c1_size       = n1 / sizeof(* c1); 
 
         for (i=0; i<c1_size; i++) {
-            c1_out[i] = _mm256_shuffle_epi8(c1[i], mask);
+            c1_out[i] = _mm_shuffle_epi8(c1[i], mask);
         }
 
-        i *= sizeof(__m256i);
+        i *= sizeof(* c1);
         for (j=0; j < c1_chars_left-1 ; j+=2) {
             ((char  *)c1_out)[i+j]    =  ((char  *)  c1)[i+j+1];
             ((char  *)c1_out)[i+j+1]  =  ((char  *)  c1)[i+j];
         }
+
         if (j < c1_chars_left)
             ((char *)c1_out)[i+j]     =  ((char  *) c1)[i+j];
 
